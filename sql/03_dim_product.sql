8ac231c8287df88bbffd74dca9f9554dbcfed689
Đỗ Hoàng Minh <minh0411369@gmail.com>
2026-09-14 19:49:33 +0700

    Add product dimension SQL

--- sql/03_dim_product.sql
+++ sql/03_dim_product.sql
@@ -1,1 +1,19 @@
+-- ============================================================
+-- Pharmacy Vietnam Sales Analytics
+-- File: 03_dim_product.sql
+-- Purpose:
+-- Create product dimension table for Power BI reporting.
+-- ============================================================
 
+CREATE OR REPLACE TABLE
+  `portfolio_project.pharmacy_vn.dim_product` AS
+
+SELECT DISTINCT
+  j_principalcode,
+  productgroupcode,
+  j_materialcode,
+  atccode,
+  principalname,
+  soldtoname
+
+FROM `portfolio_project.pharmacy_vn.all_sales`;
