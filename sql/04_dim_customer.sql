86eaf623f14163634dea8439c7387cbb51126d7b
Đỗ Hoàng Minh <minh0411369@gmail.com>
2026-09-14 19:49:59 +0700

    Add customer dimension SQL

--- sql/04_dim_customer.sql
+++ sql/04_dim_customer.sql
@@ -1,1 +1,16 @@
+-- ============================================================
+-- Pharmacy Vietnam Sales Analytics
+-- File: 04_dim_customer.sql
+-- Purpose:
+-- Create customer dimension table for Power BI reporting.
+-- ============================================================
 
+CREATE OR REPLACE TABLE
+  `portfolio_project.pharmacy_vn.dim_customer` AS
+
+SELECT DISTINCT
+  soldtoname,
+  soldtocity,
+  imschannel1desc AS channel
+
+FROM `portfolio_project.pharmacy_vn.all_sales`;
